a0a688218c1a3e508b87646afaa9d881e2e8b0b0
Jonathan <kim.jonathan426@gmail.com>
2022-08-05 18:26:22 -0400

    confirmed password field and check added

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -21,8 +21,16 @@ def username_exists(form, field):
         raise ValidationError('Username is already in use.')
 
 
+def password_checker(form, field):
+    # Checking if confirmed password matches password field
+    confirmed_password = field.data
+    password = form.data['password']
+    if (confirmed_password != password):
+        raise ValidationError('Password and Confirmed Password field do not match.')
+
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), Email(), user_exists])
     password = StringField('password', validators=[DataRequired()])
+    confirmedPassword = StringField('confirm password', validators=[DataRequired(), password_checker])
