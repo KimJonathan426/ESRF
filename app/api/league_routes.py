4a5eee8c5eec3d623c15d56593546fb3dd9c74de
Jonathan <kim.jonathan426@gmail.com>
2022-08-07 23:55:16 -0400

    Leagues reducer case, thunk, action creator, and constant for READ

--- app/api/league_routes.py
+++ app/api/league_routes.py
@@ -8,4 +8,4 @@ league_routes = Blueprint('leagues', __name__)
 @login_required
 def leagues():
     leagues = League.query.all()
-    return {'leagues': [league.to_dict() for league in leagues]}
+    return [league.to_dict() for league in leagues]
---
+++ react-app/src/store/league.js
@@ -0,0 +1,38 @@
+// Read
+const GET_LEAGUES = 'league/GET_LEAGUES';
+
+
+// Thunk Action Creators
+const actionGetLeagues = (leagues) => ({
+    type: GET_LEAGUES,
+    leagues
+})
+
+
+// Thunks
+export const getAllLeagues = () => async (dispatch) => {
+    const response = await fetch(`api/leagues/`);
+
+    if (response.ok) {
+        const leagues = await response.json();
+        dispatch(actionGetLeagues(leagues));
+        return leagues
+    }
+}
+
+
+// League Reducer
+const LeaguesReducer = (state = {}, action) => {
+    switch (action.type) {
+        case GET_LEAGUES:
+            const newState1 = {};
+            action.leagues.forEach(league => {
+                newState1[league.id] = league;
+            })
+            return newState1
+        default:
+            return state;
+    }
+}
+
+export default LeaguesReducer
