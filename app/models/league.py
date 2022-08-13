258c6be6187c19d3279d1ae75a2b3ab5ae3a9877
Jonathan <kim.jonathan426@gmail.com>
2022-08-12 21:02:41 -0400

    added start_standard column for UTC/GMT time

--- app/models/league.py
+++ app/models/league.py
@@ -17,6 +17,7 @@ class League(db.Model):
     team_player_limit = db.Column(db.Integer, nullable=False)
     start_date = db.Column(db.String(11), nullable=True)
     start_time = db.Column(db.String(11), nullable=True)
+    start_standard = db.Column(DateTime(timezone=True), nullable=True)
     field_goal_made_weight = db.Column(db.Integer, nullable=True, default=2)
     field_goal_attempted_weight = db.Column(db.Integer, nullable=True, default=-1)
     free_throw_made_weight = db.Column(db.Integer, nullable=True, default=1)
