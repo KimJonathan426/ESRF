3c1c151967addeed94f269e19e7ba604969ed69f
Jonathan <kim.jonathan426@gmail.com>
2022-08-14 10:55:55 -0400

    leaguelist component and styling complete

--- react-app/src/components/LeagueList/index.js
+++ react-app/src/components/LeagueList/index.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getPublicLeagues } from '../../store/league';
+import './LeagueList.css';
 
 const LeagueList = ({ sessionUser }) => {
     const dispatch = useDispatch();
@@ -16,8 +17,12 @@ const LeagueList = ({ sessionUser }) => {
         <div className='page-outer'>
             <div className='page-spacer'></div>
             <div className='page-container'>
+                <div className='public-title-container'>
+                    <div className='public-league-title'>Leagues can vary from recreational organizations, pick-up games, to any custom format you can think of!</div>
+                    <div className='public-league-title'>Choose from one of 10 random leagues below and explore what the community has to offer!</div>
+                </div>
                 {leagueList ?
-                    <div>
+                    <div className='public-leagues-container'>
                         {leagueList.map(league => (
                             <div key={league.id} className='my-league'>
                                 <Link to={`/leagues/${league.id}`}>
