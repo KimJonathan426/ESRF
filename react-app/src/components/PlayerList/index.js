d1e0bbf9c07cdffdfe210868c93af9147e03dbfc
Jonathan <kim.jonathan426@gmail.com>
2022-08-15 05:42:53 -0400

    fixed all table warnings and added owner check for functionality

--- react-app/src/components/PlayerList/index.js
+++ react-app/src/components/PlayerList/index.js
@@ -5,16 +5,20 @@ import { getAllPlayers, clearPlayers } from "../../store/player";
 import PlayerModal from "../PlayerModal";
 import EditPlayerModal from "../EditPlayerModal";
 import DeletePlayerModal from "../DeletePlayerModal";
+import { getSingleLeague } from "../../store/league";
 import './PlayerList.css';
 
-const PlayerList = () => {
+const PlayerList = ({ sessionUser }) => {
     const { leagueId } = useParams();
     const dispatch = useDispatch();
     const players = useSelector(state => state.players);
+    const leagueState = useSelector(state => state.leagues);
+    const league = leagueState[leagueId];
     const playerList = Object.values(players);
 
     useEffect(() => {
         dispatch(getAllPlayers(leagueId))
+        dispatch(getSingleLeague(leagueId))
 
         return () => dispatch(clearPlayers())
     }, [dispatch, leagueId])
@@ -28,66 +32,83 @@ const PlayerList = () => {
                 <div className='free-agents-title'>
                     Free Agents
                     <span>Browse all available players below...</span>
-                    </div>
+                </div>
                 {playerList ?
                     <div className='free-agents-container'>
                         <table className='free-agents-table'>
-                            <tr className='player-column-headers'>
-                                <th className='column-1'>PLAYERS</th>
-                                <th className='column-2'>STATS</th>
-                                <th className='column-3'>FANTASY PTS</th>
-                            </tr>
-                            <tr className='player-column-sub-headers'>
-                                <th className='sub-column-1'>PLAYER</th>
-                                <th className='sub-column-2'>
-                                    <td className='sub-column-2-columns'>FGM</td>
-                                    <td className='sub-column-2-columns'>FGA</td>
-                                    <td className='sub-column-2-columns'>FTM</td>
-                                    <td className='sub-column-2-columns'>FTA</td>
-                                    <td className='sub-column-2-columns'>3PM</td>
-                                    <td className='sub-column-2-columns'>AST</td>
-                                    <td className='sub-column-2-columns'>REB</td>
-                                    <td className='sub-column-2-columns'>STL</td>
-                                    <td className='sub-column-2-columns'>BLK</td>
-                                    <td className='sub-column-2-columns'>TO</td>
-                                    <td className='sub-column-2-columns'>PTS</td>
-                                </th>
-                                <th className='sub-column-3'>TOT</th>
-                            </tr>
+                            <thead>
 
-                            {playerList.map(player => (
-                                <tr key={player.id} className='individual-player-row'>
-                                    <td className='player-column'>
-                                        <div className='player-info-box'>
-                                            <img src={`${player.player_image}`} alt='player'></img>
-                                            <div>
-                                                <PlayerModal player={player} />
-                                                <span>{player.position}</span>
-                                            </div>
-                                        </div>
-                                        <div className='player-action-box'>
-                                            <EditPlayerModal player={player} />
-                                            <DeletePlayerModal totalPlayers={playerList.length} playerId={player.id} />
-                                        </div>
-                                    </td>
-                                    <td className='player-stat-column'>
-                                        <td className='sub-column-2-columns'>{player.field_goal_made}</td>
-                                        <td className='sub-column-2-columns'>{player.field_goal_attempted}</td>
-                                        <td className='sub-column-2-columns'>{player.free_throw_made}</td>
-                                        <td className='sub-column-2-columns'>{player.free_throw_attempted}</td>
-                                        <td className='sub-column-2-columns'>{player.three_point_made}</td>
-                                        <td className='sub-column-2-columns'>{player.assists}</td>
-                                        <td className='sub-column-2-columns'>{player.rebounds}</td>
-                                        <td className='sub-column-2-columns'>{player.steals}</td>
-                                        <td className='sub-column-2-columns'>{player.blocks}</td>
-                                        <td className='sub-column-2-columns'>{player.turnovers}</td>
-                                        <td className='sub-column-2-columns'>{player.points}</td>
-                                    </td>
-                                    <td className='fantasy-total-column'>
-                                        {player.fantasy_total}
-                                    </td>
+                                <tr className='player-column-headers'>
+                                    <th className='column-1'>PLAYERS</th>
+                                    <th className='column-2'>STATS</th>
+                                    <th className='column-3'>FANTASY PTS</th>
                                 </tr>
-                            ))}
+                                <tr className='player-column-sub-headers'>
+                                    <th className='sub-column-1'>PLAYER</th>
+                                    <th className='sub-column-2'>
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td className='sub-column-2-columns'>FGM</td>
+                                                    <td className='sub-column-2-columns'>FGA</td>
+                                                    <td className='sub-column-2-columns'>FTM</td>
+                                                    <td className='sub-column-2-columns'>FTA</td>
+                                                    <td className='sub-column-2-columns'>3PM</td>
+                                                    <td className='sub-column-2-columns'>AST</td>
+                                                    <td className='sub-column-2-columns'>REB</td>
+                                                    <td className='sub-column-2-columns'>STL</td>
+                                                    <td className='sub-column-2-columns'>BLK</td>
+                                                    <td className='sub-column-2-columns'>TO</td>
+                                                    <td className='sub-column-2-columns'>PTS</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </th>
+                                    <th className='sub-column-3'>TOT</th>
+                                </tr>
+
+                                {playerList.map(player => (
+                                    <tr key={player.id} className='individual-player-row'>
+                                        <td className='player-column'>
+                                            <div className='player-info-box'>
+                                                <img src={`${player.player_image}`} alt='player'></img>
+                                                <div>
+                                                    <PlayerModal player={player} />
+                                                    <span>{player.position}</span>
+                                                </div>
+                                            </div>
+                                            {league.owner_id === sessionUser.id && (
+                                                <div className='player-action-box'>
+                                                    <EditPlayerModal player={player} />
+                                                    <DeletePlayerModal totalPlayers={playerList.length} playerId={player.id} />
+                                                </div>
+                                            )}
+                                        </td>
+                                        <td className='player-stat-column'>
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <td className='sub-column-2-columns'>{player.field_goal_made}</td>
+                                                        <td className='sub-column-2-columns'>{player.field_goal_attempted}</td>
+                                                        <td className='sub-column-2-columns'>{player.free_throw_made}</td>
+                                                        <td className='sub-column-2-columns'>{player.free_throw_attempted}</td>
+                                                        <td className='sub-column-2-columns'>{player.three_point_made}</td>
+                                                        <td className='sub-column-2-columns'>{player.assists}</td>
+                                                        <td className='sub-column-2-columns'>{player.rebounds}</td>
+                                                        <td className='sub-column-2-columns'>{player.steals}</td>
+                                                        <td className='sub-column-2-columns'>{player.blocks}</td>
+                                                        <td className='sub-column-2-columns'>{player.turnovers}</td>
+                                                        <td className='sub-column-2-columns'>{player.points}</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                        <th className='fantasy-total-column'>
+                                            {player.fantasy_total}
+                                        </th>
+                                    </tr>
+                                ))}
+                            </thead>
                         </table>
                         <div className='table-spacer'></div>
                     </div>
