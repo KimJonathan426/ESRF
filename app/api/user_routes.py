3d3f3ce407aa41a31a526b5ad77bb455a61f2bbc
Jonathan <kim.jonathan426@gmail.com>
2022-10-05 22:33:08 -0400

    added route to find all users in a league

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, League
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +17,15 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/<int:leagueId>/all')
+@login_required
+def all_users(leagueId):
+    league = League.query.get(leagueId)
+    teams = league.to_dict()['teams']
+
+    user_ids = [team['team_owner_id'] for team in teams]
+
+    users = User.query.filter(User.id.in_(user_ids)).all()
+
+    return {'users': [user.to_dict() for user in users]}
